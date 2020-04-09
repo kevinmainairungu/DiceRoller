8ca6a375761c1d94a311dd7c992532bfcf119128
kevinmainairungu <mainairungu99@gmail.com>
2020-04-09 15:04:09 +0300

    dynamic changing string values and assigning findviewbyid

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val roll_button : Button = findViewById(R.id.roll_button)
     }
 }
